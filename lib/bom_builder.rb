d0078b621fb34d7e6a43e9c46d9cfedfa0457e69
joseph kobti <j.kobti@vehiculum.de>
2019-02-27 10:11:45 +0100

    fix indent

--- lib/bom_builder.rb
+++ lib/bom_builder.rb
@@ -6,51 +6,48 @@ require "rest_client"
 require_relative "bom_helpers"
 
 class Bombuilder
-    def self.build(path)
-        setup(path)
-        specs_list
-        bom = build_bom(@gems)
-        File.open("bom.xml", "w") {|file| file.write(bom)}
+  def self.build(path)
+    setup(path)
+    specs_list
+    bom = build_bom(@gems)
+    File.open("bom.xml", "w") {|file| file.write(bom)}
+  end
+  private
+  def self.setup(path)
+    @gems = []
+    licenses_file = File.read "lib/licenses.json"
+    @licenses_list = JSON.parse(licenses_file)
+    abort("missing path to project directory") if path.nil?
+    
+    begin
+      Dir.chdir path
+      gemfile = File.read("Gemfile.lock")
+      @specs = Bundler::LockfileParser.new(gemfile).specs
+    rescue 
+      abort "Input argument should refer to a valid Ruby on Rails project folder"
     end
-
-    private
-    def self.setup(path)
-        @gems = []
-
-        licenses_file = File.read "lib/licenses.json"
-        @licenses_list = JSON.parse(licenses_file)
-
-        abort("missing path to project directory") if path.nil?
-        
-        begin
-            Dir.chdir path
-            gemfile = File.read("Gemfile.lock")
-            @specs = Bundler::LockfileParser.new(gemfile).specs
-        rescue 
-            abort "Input argument should refer to a valid Ruby on Rails project folder"
-        end
-
-    end
-    def self.specs_list
-      count = 0
-      @specs.each do |dependency|
-        puts @licenses
-        object = OpenStruct.new
-        object.name = dependency.name 
-        object.version = dependency.version 
-        object.purl = purl(object.name, object.version)
-        gem = get_gem(object.name, object.version)
-        next if gem.nil?
-        if gem["licenses"] and @licenses_list.include? gem["licenses"].first
-          object.license = gem["licenses"].first
-        end
-        object.author = gem["authors"]
-        object.description = gem["summary"] 
-        object.hash = gem["sha"]
-        @gems.push(object)
-        count += 1
-        puts "#{object.name}:#{object.version} gem added"
+  end
+  
+  def self.specs_list
+    count = 0
+    @specs.each do |dependency|
+      puts @licenses
+      object = OpenStruct.new
+      object.name = dependency.name 
+      object.version = dependency.version 
+      object.purl = purl(object.name, object.version)
+      gem = get_gem(object.name, object.version)
+      next if gem.nil?
+      if gem["licenses"] and @licenses_list.include? gem["licenses"].first
+        object.license = gem["licenses"].first
       end
-      puts "#{count} gems were added"
-    end 
+      object.author = gem["authors"]
+      object.description = gem["summary"] 
+      object.hash = gem["sha"]
+      @gems.push(object)
+      count += 1
+      puts "#{object.name}:#{object.version} gem added"
+    end
+    puts "#{count} gems were added"
+  end 
 end
--- lib/bom_helpers.rb
+++ lib/bom_helpers.rb
@@ -3,42 +3,42 @@ def purl(name, version)
 end
 
 def build_bom(gems)
-    builder = Nokogiri::XML::Builder.new(:encoding => "UTF-8") do |xml|
-        attributes = {"xmlns" => "http://cyclonedx.org/schema/bom/1.0","xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance", "version" => "1", "xsi:schemaLocation" => "http://cyclonedx.org/schema/bom/1.0 http://cyclonedx.org/schema/bom/1.0"}
-        xml.bom(attributes) do
-            xml.components {
-                gems.each do |gem|
-                    xml.component("type" => "library") {
-                        xml.name gem["name"]
-                        xml.version gem["version"]
-                        xml.description gem["description"]
-                        xml.hashes{
-                            xml.hash_ gem["hash"], :alg => "SHA-256"
-                        }
-                        if gem["license"]
-                            xml.licenses {
-                                xml.license{
-                                    xml.id gem["license"]
-                                }
-                            }
-                        end
-                        xml.purl gem["purl"]
-                        xml.modified "false" 
-                    }
-                end
+  builder = Nokogiri::XML::Builder.new(:encoding => "UTF-8") do |xml|
+    attributes = {"xmlns" => "http://cyclonedx.org/schema/bom/1.0","xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance", "version" => "1", "xsi:schemaLocation" => "http://cyclonedx.org/schema/bom/1.0 http://cyclonedx.org/schema/bom/1.0"}
+    xml.bom(attributes) do
+      xml.components {
+        gems.each do |gem|
+          xml.component("type" => "library") {
+            xml.name gem["name"]
+            xml.version gem["version"]
+            xml.description gem["description"]
+            xml.hashes{
+              xml.hash_ gem["hash"], :alg => "SHA-256"
             }
+            if gem["license"]
+              xml.licenses {
+                xml.license{
+                  xml.id gem["license"]
+                }
+              }
+            end
+            xml.purl gem["purl"]
+            xml.modified "false" 
+          }
         end
-    end 
-    builder.to_xml
+      }
+    end
+  end 
+  builder.to_xml
 end 
 
 def get_gem(name, version)
-    url = "https://rubygems.org/api/v1/versions/#{name}.json"
-    begin
-        response = RestClient.get(url)
-        body = JSON.parse(response.body)
-        body.select {|item| item["number"] == version.to_s}.first
-    rescue 
-        return nil
-    end
+  url = "https://rubygems.org/api/v1/versions/#{name}.json"
+  begin
+    response = RestClient.get(url)
+    body = JSON.parse(response.body)
+    body.select {|item| item["number"] == version.to_s}.first
+  rescue 
+    return nil
+  end
 end 
