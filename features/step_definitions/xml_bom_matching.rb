4836115c337da73e6a71a5be209738c22057f6dc
M. Scott Ford <scott@corgibytes.com>
2023-04-02 14:46:57 -0400

    Adds coverage for using `--path` with default options
    Signed-off-by: M. Scott Ford <scott@corgibytes.com>

---
+++ features/step_definitions/xml_bom_matching.rb
@@ -0,0 +1,11 @@
+Then('the generated XML BOM file {string} matches {string}') do |generated_file, expected_file|
+  generated_file_contents = File.read(expand_path(generated_file))
+  expected_file_contents = File.read(expand_path(expected_file))
+
+  serial_number_matcher = /serialNumber=\"urn:uuid:[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}\"/
+  normalized_serial_number = 'serialNumber="00000000-0000-0000-0000-000000000000"'
+  normalized_generated_file_contents = generated_file_contents.gsub(serial_number_matcher, normalized_serial_number)
+  normalized_expected_file_contents = expected_file_contents.gsub(serial_number_matcher, normalized_serial_number)
+
+  expect(normalized_expected_file_contents).to eq(normalized_generated_file_contents)
+end
